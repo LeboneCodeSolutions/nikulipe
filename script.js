2780282951445c59cee280634c324c1fdf43c767
masterlebone <68324851+masterlebone@users.noreply.github.com>
2023-09-14 09:28:33 +0200

    updated content 14 september

--- script.js
+++ script.js
@@ -5,10 +5,19 @@ import { fileURLToPath } from "url";
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(__dirname)); // This line serves static files from the current directory
+app.use(express.static(__dirname));
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+const commonHandler = (req, res) => {
+    res.sendFile(__dirname + '/public/404-page.html');
+};
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// Handle similar routes with a loop
+['/solutions', '/insights', '/company', '/contact_us'].forEach(route => {
+    app.get(route, commonHandler);
 });
 
 app.listen(3000);
